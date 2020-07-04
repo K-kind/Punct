a0f1f2cbce6bc545d02d1345995ba8dfdc2d7c31
K.K <gokennsirou@yahoo.co.jp>
2020-07-04 22:06:07 +0900

    [Add] user create action and user model validations

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,3 +1,19 @@
 class UsersController < ApplicationController
+  def create
+    user = User.new(user_params)
+    if user.save
+      payload = { message: '登録が完了しました。', name: user.name }
+    else
+      payload = { error: user.errors.full_messages.join("\n") }
+    end
+    render json: payload
+  end
+
   def show; end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :email_confirmation, :password, :password_confirmation)
+  end
 end
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   has_many :tasks
   has_many :weekly_tasks
   has_many :monthly_tasks
+
+  validates :name, presence: true,
+                   length: { maximum: 8 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true,
+                    length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false },
+                    confirmation: true
+  validates :email_confirmation, presence: true
+  validates :password, presence: true,
+                       length: { minimum: 6 },
+                       allow_nil: true
+  validates :password_confirmation, presence: true
 end
