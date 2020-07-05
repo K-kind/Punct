490ee463772df20a50993f7e08bc12355a34a7b9
K.K <gokennsirou@yahoo.co.jp>
2020-07-05 13:21:00 +0900

    [Update] element plugin to be loaded on-demand

--- frontend/src/plugins/element.js
+++ frontend/src/plugins/element.js
@@ -1,6 +1,22 @@
-import Vue from 'vue'
-import Element from 'element-ui'
-import '../element-variables.scss'
-import locale from 'element-ui/lib/locale/lang/ja'
+// import Vue from 'vue'
+// import Element from 'element-ui'
+// import '../element-variables.scss'
+// import locale from 'element-ui/lib/locale/lang/ja'
 
-Vue.use(Element, { locale })
+// Vue.use(Element, { locale })
+
+import Vue from 'vue'
+import {
+  Button,
+  Input,
+  Notification,
+} from 'element-ui'
+import lang from 'element-ui/lib/locale/lang/ja'
+import locale from 'element-ui/lib/locale'
+
+locale.use(lang)
+
+Vue.use(Button)
+Vue.use(Input)
+
+Vue.prototype.$notify = Notification;
