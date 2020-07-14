c5d3f0fc13198d131af0c6986bbfb63fc7f0a5f1
K.K <gokennsirou@yahoo.co.jp>
2020-07-15 07:01:30 +0900

    [Refactor] router routes and global guards

---
+++ frontend/src/router/routes.js
@@ -0,0 +1,55 @@
+import Home from '@/views/Home.vue'
+import Login from '@/views/Login.vue'
+
+export default [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/archives',
+    name: 'Archives',
+    component: () => import('@/views/Archives.vue')
+  },
+  {
+    path: '/mypage',
+    name: 'MyPage',
+    component: () => import('@/views/MyPage.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login,
+    meta: {
+      isPublic: true,
+      forGuest: true
+    }
+  },
+  {
+    path: '/signup',
+    name: 'SignUp',
+    component: () => import('@/views/SignUp.vue'),
+    meta: {
+      isPublic: true,
+      forGuest: true
+    }
+  },
+  {
+    path: '/oauth',
+    name: 'Oauth',
+    component: () => import('@/views/Oauth.vue'),
+    meta: {
+      isPublic: true
+    }
+  },
+  {
+    path: '/reset',
+    name: 'Reset',
+    component: () => import('@/views/Reset.vue'),
+    meta: {
+      isPublic: true,
+      forGuest: true
+    }
+  }
+]
