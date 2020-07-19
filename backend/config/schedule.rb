c9486fdc52a5705f1c4ef7a6bd5ea98d4c471674
K.K <gokennsirou@yahoo.co.jp>
2020-07-19 21:08:38 +0900

    [Add] whenever schedule

--- backend/config/schedule.rb
+++ backend/config/schedule.rb
@@ -3,6 +3,7 @@
 rails_env = ENV['RAILS_ENV'] || :development
 set :environment, rails_env
 # set :output, "#{Rails.root}/log/cron.log"
+# set :output, "./log/cron.log"
 # env :PATH, ENV['PATH']
 
 # every '0 0 * * *' do # 毎日0時0分
@@ -15,7 +16,7 @@ set :environment, rails_env
 #   runner 'lib/tasks/update_test_user.rb', :environment_variable => 'RAILS_ENV'
 # end
 
-every '0 4 * * *' do # 毎日4時
-  command 'echo [CRONTAB LOGGER] `date`'
+every '0 4,15 * * *' do # 毎日4, 15時
+  runner 'puts "[CRONTAB LOGGER] #{Time.zone.now}"'
   runner 'lib/tasks/clean_test_users.rb'
 end
