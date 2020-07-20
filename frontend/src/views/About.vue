35e9fcafeab73e11999e75291a29311ab5e23962
K.K <gokennsirou@yahoo.co.jp>
2020-07-20 16:33:00 +0900

    [Add] AboutInfo component to integrate redundunt templates of About and Help components

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,20 +1,6 @@
 <template>
   <div class="container">
-    <div class="about__header">
-      <h1 class="about__heading">Punct<span>（パンクト）</span></h1>
-      <p class="about__text">
-        Punctual（パンクチュアル：時間に正確）な行動をサポートするタスク管理アプリです。<br>
-        3ステップでタスクに1つずつ取り組み、かかった時間を記録します。
-      </p>
-      <ul class="about__steps">
-        <li>内容と予定時間を入力してタスクを作成</li>
-        <li>「進行中」にタスクをセットし、タイマーを開始</li>
-        <li>完了ボタンを押して、経過時間を保存</li>
-      </ul>
-    </div>
-    <div class="about__video">
-      <video src="@/assets/capture.mp4" controls></video>
-    </div>
+    <AboutInfo />
     <div class="about__bottom-links">
       <router-link to="/signup" class="about__signup-link">
         <i class="el-icon-caret-right"></i>
@@ -31,11 +17,13 @@
 
 <script>
 import TestLogin from '@/components/TestLogin.vue'
+import AboutInfo from '@/components/AboutInfo.vue'
 
 export default {
   name: 'About',
   components: {
-    TestLogin
+    TestLogin,
+    AboutInfo
   }
 }
 </script>
@@ -51,32 +39,6 @@ p, li, a {
   line-height: 1.8;
 }
 .about {
-  &__header {
-    padding-bottom: 24px;
-  }
-  &__heading {
-    text-align: center;
-    span {
-      font-size: 1.4rem;
-    }
-  }
-  // &__text {
-  // }
-  &__steps {
-    padding-top: 16px;
-    padding-left: 16px;
-    li {
-      list-style: decimal;
-    }
-  }
-  &__video {
-    width: 60%;
-    video {
-      width: 100%;
-      border: 1px solid $theme-gray;
-      border-bottom-width: 2px;
-    }
-  }
   &__bottom-links {
     padding: 32px 0;
     text-align: center;
