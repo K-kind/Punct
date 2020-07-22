a4b4106cbd20636ff88fd0e4cfa3e9f84838427b
K.K <gokennsirou@yahoo.co.jp>
2020-07-22 19:22:24 +0900

    [Add] admin mailer contact action

--- backend/app/controllers/chores_controller.rb
+++ backend/app/controllers/chores_controller.rb
@@ -2,6 +2,11 @@ class ChoresController < ApplicationController
   skip_before_action :require_login, except: [:destroy]
 
   def contact
+    AdminMailer.contact(
+      contact: params[:contact],
+      email: params[:email],
+      user: current_user
+    ).deliver_later
     render json: { message: 'お問い合わせを送信しました。' }
   end
 end
