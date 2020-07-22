7394577ad8699e35231e3c985755f1043224bac3
K.K <gokennsirou@yahoo.co.jp>
2020-07-22 09:06:05 +0900

    [Add] outline of ReloadNotify component and fix remaining timer bug

---
+++ frontend/src/components/ReloadNotify.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="home">
+  </div>
+</template>
+
+<script>
+const setTimer = (vm) => {
+  setInterval(() => {
+    console.log(vm.$dayjs().$s)
+  }, 1000)
+  // setInterval(() => {
+  //   vm.$dayjs.isAfter
+  // }, 1000 * 60)
+  // this.timerId = setInterval(() => {
+  //   self.computeElapsedTime()
+  // }, 1000)
+}
+
+export default {
+  name: 'ReloadNotify',
+  data() {
+    return {
+      createdDate: (new Date).toLocaleDateString(),
+      dateIsUpdated: false,
+    }
+  },
+  // computed: {
+  //   weekString() {
+  //     let range = this.$store.state.weekly.weekString || '6日間'
+  //     return `${range}のタスク`
+  //   }
+  // },
+  created() {
+    setTimer
+    // setTimer(this)
+  },
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
